d7308434c93eab07a8ce4387452b8d2f1b77fd40
hesbornO <46963355+hesbornO@users.noreply.github.com>
2022-11-16 14:20:29 +0300

    Create built-in.c

---
+++ built-in.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void built_in(char *string, char **argv, char **env, int *ex_it)
+{
+	if (_strcmp(argv[0], "exit") == 0)
+	{
+		free(argv);
+		free(string);
+		exit(*ex_it);
+	}
+	if (_strcmp(argv[0], "env") == 0)
+		printenv(env, ex_it);
+}
