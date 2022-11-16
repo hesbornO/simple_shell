a111d8bcdd62bc759c40357ececa673c32dc2c71
kelvinmudanya <32190945+kelvinmudanya@users.noreply.github.com>
2022-11-16 14:57:10 +0300

    Update built-in.c

--- built-in.c
+++ built-in.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 void built_in(char *string, char **argv, char **env, int *ex_it)
 {
 	if (_strcmp(argv[0], "exit") == 0)
 	{
 		free(argv);
 		free(string);
+		
 		exit(*ex_it);
 	}
 	if (_strcmp(argv[0], "env") == 0)
